2c2670434ff390cc9add5f3073b6f1a44d3a38ad
John Ferguson <JAFerguson952@gmail.com>
2024-08-20 13:41:18 +0100

    added some TOPO_LOG values for loggin

--- activate.sh
+++ activate.sh
@@ -2,5 +2,5 @@
 export TOPOHEDRAL_VIEWER_HOME=$(pwd)
 export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/html/custom-header.html --document-private-items"
 export TOPOHEDRAL_VIEWER_DEV=1
-export RUST_LOG=topohedral_viewer=trace
+export TOPO_LOG=all=debug,update_uniform=trace
 export PYTHONSTARTUP=$TOPOHEDRAL_VIEWER_HOME/startup.py
