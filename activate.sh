113a29f63c1c8f6ebdbfd356505dd9a8228be848
John Ferguson <JAFerguson952@gmail.com>
2024-08-21 17:05:24 +0100

    changed the logging level

--- activate.sh
+++ activate.sh
@@ -2,5 +2,5 @@
 export TOPOHEDRAL_VIEWER_HOME=$(pwd)
 export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/html/custom-header.html --document-private-items"
 export TOPOHEDRAL_VIEWER_DEV=1
-export TOPO_LOG=all=debug,update_uniform=trace
+export TOPO_LOG=all=info,update_uniform=trace
 export PYTHONSTARTUP=$TOPOHEDRAL_VIEWER_HOME/startup.py
