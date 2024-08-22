175443ca9c4e8278a99bd927e84db51b602a42c8
John Ferguson <JAFerguson952@gmail.com>
2024-08-23 00:17:34 +0100

    added command for compiling d3 proto

--- build.rs
+++ build.rs
@@ -1,4 +1,16 @@
+//! The purpose of this build script is to generate the Rust code for the RPC services defined 
+//! in the `protos` directory.
+//!
+//! The protos are compiled using the `tonic` crate, which generates Rust code from the proto files.
+//--------------------------------------------------------------------------------------------------
 
+//{{{ crate imports 
+//}}}
+//{{{ std imports 
+//}}}
+//{{{ dep imports 
+//}}}
+//--------------------------------------------------------------------------------------------------
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {   
 
@@ -7,6 +19,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(true)
         .out_dir("src/d2/rpc")
         .compile(&["protos/d2.proto"], &["protos"])?;
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .out_dir("src/d3/rpc")
+        .compile(&["protos/d3.proto"], &["protos"])?;
 
     Ok(())
 }
