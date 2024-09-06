f6b11920039bf4e1f8791d5b2a99a57845485e5a
John Ferguson <JAFerguson952@gmail.com>
2024-09-06 23:22:52 +0100

    changed trace to focus on test

--- activate.sh
+++ activate.sh
@@ -2,5 +2,5 @@
 export TOPOHEDRAL_VIEWER_HOME=$(pwd)
 export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/html/custom-header.html --document-private-items"
 export TOPOHEDRAL_VIEWER_DEV=1
-export TOPO_LOG=all=trace,update_uniform=trace
+export TOPO_LOG=d2_rpc_test=trace
 export PYTHONSTARTUP=$TOPOHEDRAL_VIEWER_HOME/startup.py
