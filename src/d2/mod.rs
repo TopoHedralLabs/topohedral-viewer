5fd5a421907aa733258b2120db35289311cee3f7
John Ferguson <JAFerguson952@gmail.com>
2024-09-12 20:26:40 +0100

    exporting LineDescriptor out of module

--- src/d2/mod.rs
+++ src/d2/mod.rs
@@ -21,5 +21,5 @@ mod rpc;
 pub(crate) use state::State;
 pub(crate) use rpc::run_server;
 
-pub use mesh::{AxesDescriptor,SquareDescriptor, CircleDescriptor, Mesh, Mesh2D};
+pub use mesh::{AxesDescriptor,LineDescriptor, SquareDescriptor, CircleDescriptor, Mesh, Mesh2D};
 pub use rpc::Client2D;
